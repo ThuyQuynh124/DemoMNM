213add9721c21f45a7ac13087a9b4bfaf994c03f
LE THUY QUYNH <1724010123@student.humg.edu.vn>
2021-11-10 00:23:44 +0700

    first commit

---
+++ DemoMNM/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DemoMNM.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
